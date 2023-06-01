a9be294a8fa778ff3b06d59ed8e0a8e07b354f56
davidjose47sudo <78758821+davidjose47sudo@users.noreply.github.com>
2023-06-01 11:24:41 -0500

    update home and login

---
+++ edu-sena/src/app/[ID]/admin/home/page.tsx
@@ -0,0 +1,14 @@
+import Nav from "@/componets/global/nav";
+import Image from "next/image";
+import Script from "next/script";
+
+export default function home() {
+    return (
+        <>
+            <div>
+                <Nav />
+            </div>
+            <h1>hola home admin</h1>
+        </>
+    )
+}
--- edu-sena/src/app/page.tsx
+++ edu-sena/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'; // ahora hay que indicar si el componenete lo renderizara el usuario o el servidor
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { FormEvent, FormEventHandler, useState } from "react";
 import styles from "../styles/page.module.css";
 
 
@@ -9,7 +9,8 @@ export default function Home() {
   const [username, setUsername]: any = useState("username");
   const [password, setPassword]: any = useState("password");
 
-  function validation() {
+  function validation(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
     if (true) {
       route.push(`/${username}/estudent/home`)
     } else {
@@ -27,49 +28,40 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-      </div>
-
-      <div className={styles.center}>
-        <div className={styles.logincontainer}>
-          <div>
-            <h1>Login</h1>
-          </div>
-          <form className={styles.containerform} onSubmit={validation}>
-            <div>
-              <div className={styles.form_group}>
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  autoComplete="off"
-                  placeholder="Enter your username"
-                  onChange={handleUsernameChange}
-                />
-              </div>
-              <div className={styles.form_group}>
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  autoComplete="off"
-                  placeholder="Enter your password"
-                  onChange={handlePasswordChange}
-                />
-              </div>
-            </div>
-            <div className={styles.containerbutton}>
-              <input type="submit" onSubmit={validation} />
-            </div>
-          </form>
+      <article className={styles.logincontainer}>
+        <div>
+          <h1>SENA LOGIN</h1>
         </div>
-      </div>
+        <form className={styles.containerform} onSubmit={validation}>
+          <div>
+            <div className={styles.form_group}>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                autoComplete="off"
+                placeholder="Enter your username"
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="off"
+                placeholder="Enter your password"
+                onChange={handlePasswordChange}
+              />
+            </div>
+          </div>
+          <div className={styles.containerbutton}>
+            <input value={"login in"} type="submit" />
+          </div>
+        </form>
+      </article>
       <div className={styles.grid}></div>
     </main>
   );
